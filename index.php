0ffcb9120d913a2e985491e28f0917fcd44b712f
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-01 22:08:42 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    if(!isset($_SESSION)){
+        session_start();
+    }
+
+    if(!isset($_SESSION['login'])){
+       header("Location: login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor WEB</title>
+</head>
+<body>
+   <p>Gestor WEB. Em Breve...</p> 
+</body>
+</html>
