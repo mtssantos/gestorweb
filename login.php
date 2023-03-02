c159155b59d99d08470b78193ea8a86c0b0d10e9
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-01 23:28:29 -0300

    Update Page Login

--- login.php
+++ login.php
@@ -5,10 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Login: Gestor Web</title>
 </head>
 <body>
-   <p>Entre para fazer login.</p>
+   <div class="container">
+        <h5 class="center-align">Entre para realizar login:</h5>
+        <form action="">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="center-align">
+                    <button class="btn waves-effect waves-light btn-large" type="submit" name="action">Entrar
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
+        </form>
+   </div>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
 </body>
 </html>
