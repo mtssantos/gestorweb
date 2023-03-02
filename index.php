6ef709b375fe83bab1e5ec017a004219b3c216d0
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-01 22:47:43 -0300

    Update Materialize

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
     if(!isset($_SESSION)){
         session_start();
     }
@@ -14,9 +13,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <title>Gestor WEB</title>
 </head>
 <body>
    <p>Gestor WEB. Em Breve...</p> 
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
