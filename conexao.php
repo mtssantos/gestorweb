ad996f74b0545327cc5d4c5e2156dd58e38b81dc
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-13 12:33:59 -0300

    Connection PHP - MySQL - 7

--- conexao.php
+++ conexao.php
@@ -1,8 +1,9 @@
 <?php
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-        
-    $con = mysqli_connect($_ENV["CONNECTION"], $_ENV["USER_ROOT"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+    use DevCoder/DotEnv;
+
+    (new DotEnv(__DIR__ . '/.env'))->load();
+
+    $con = mysqli_connect(getenv["CONNECTION"], getenv["USER_ROOT"], getenv["PASSWORD"], getenv["DATABASE"]);
 
     if (!$con){
         echo "Erro de acesso ao banco de dados!";
