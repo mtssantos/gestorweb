4c8943538b3032d39acffcce51b75ed44737c510
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-15 18:13:49 -0300

    Update acesso

--- login.php
+++ login.php
@@ -60,7 +60,6 @@
             </div>
         </form>
    </div>
-   <?php echo $loginerro; ?>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
 </body>
 </html>
