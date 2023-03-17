175d3b6a3f6066bd50ccbc3bd346d406835d61f4
mateus-costa33 <soutecinformatica1@gmail.com>
2023-03-17 23:14:38 +0000

    Update Style Text

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
 </head>
 <body> 
     <div class="container-md">
-        <div class="arealogin w-200">
+        <div class="arealogin w-75">
             <form>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
